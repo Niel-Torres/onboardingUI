a8c91fad408234699c058d44765b87de4908361b
Niel Torres <ntorrema@nttdata.com>
2023-11-17 00:40:35 +0100

    Initial Commit

---
+++ OnboardingUI/OnboardingUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingUIApp.swift
+//  OnboardingUI
+//
+//  Created by Odon Niel Torres Mariluz on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
